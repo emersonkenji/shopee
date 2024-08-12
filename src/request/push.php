da951e95d97b33c20d58d03c0e288f057fba1483
Faiznurullah <Faizn103a@gmail.com>
2024-08-12 11:51:57 +0700

    Push

---
+++ src/request/push.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Faiznurullah\Shopee;
+
+use Faiznurullah\Shopee\config;
+
+class push extends config{
+
+    private $config, $partnerid, $partnerkey, $shopee, $access_token;
+
+    public function __construct($partnerid, $partnerkey)
+    {
+        $this->partnerid = $partnerid;
+        $this->partnerkey = $partnerkey;
+        $this->shopee = new shopee();
+    }
+
+    public function setAppPushConfig($data = []){ 
+        $sign = parent::getSign();
+        $suburl = '/push/set_app_push_config?partner_id='.$this->partnerid.'&sign='.$sign.'&timestamp='.$this->timest;
+        $response = $this->shopee->postMethod($suburl, $data);
+        return $response;
+    }
+
+    public function getAppPushConfig(){
+        $sign = parent::getSign();
+        $suburl = '/push/get_app_push_config?partner_id='.$this->partnerid.'&sign='.$sign.'&timestamp='.$this->timest;
+        $response = $this->shopee->getMethod($suburl);
+        return $response;
+    }
+
+    public function getLostPushMessage(){
+        $sign = parent::getSign();
+        $suburl = '/push/get_lost_push_message?partner_id='.$this->partnerid.'&sign='.$sign.'&timestamp='.$this->timest;
+        $response = $this->shopee->getMethod($suburl);
+        return $response;
+    }
+
+    public function confirmConsumedLostPushMessage(){
+        $sign = parent::getSign();
+        $suburl = '/push/confirm_consumed_lost_push_message?partner_id='.$this->partnerid.'&sign='.$sign.'&timestamp='.$this->timest;
+        $response = $this->shopee->getMethod($suburl);
+        return $response;
+    }
+
+}
