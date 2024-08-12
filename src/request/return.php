7a9d596b3da7b2e531f7b03374f669b11d1ee3d3
Faiz Nurullah <59213454+Faiznurullah@users.noreply.github.com>
2024-08-12 19:57:31 +0700

    return feture

--- src/request/return.php
+++ src/request/return.php
@@ -65,6 +65,38 @@
         return $response;
     }
 
-   
+    public function convertImage($authcode, $shop_id, $data = []){
+        $access_token = parent::getAccesToken($authcode, $shop_id);
+        $sign = parent::getSign();
+        $suburl = '/returns/convert_image?access_token='.$access_token.'&partner_id='.$this->partnerid.'&shop_id='.$shop_id.'&sign='.$sign.'&timestamp='.$this->timest;
+        $response = $this->shopee->postMethod($suburl, $data);  
+        return $response;
+    }
+
+    public function uploadProof($authcode, $shop_id, $data = []){
+        $access_token = parent::getAccesToken($authcode, $shop_id);
+        $sign = parent::getSign();
+        $suburl = '/returns/upload_proof?access_token='.$access_token.'&partner_id='.$this->partnerid.'&shop_id='.$shop_id.'&sign='.$sign.'&timestamp='.$this->timest;
+        $response = $this->shopee->postMethod($suburl, $data);
+        return $response;
+    }
+
+    public function queryProof($authcode, $shop_id){
+        $access_token = parent::getAccesToken($authcode, $shop_id);
+        $sign = parent::getSign();
+        $suburl = '/returns/query_proof?access_token='.$access_token.'&partner_id='.$this->partnerid.'&shop_id='.$shop_id.'&sign='.$sign.'&timestamp='.$this->timest;
+        $response = $this->shopee->getMethod($suburl);
+        return $response;
+    }
+
+
+    public function getReturnDispute($authcode, $shop_id){
+        $access_token = parent::getAccesToken($authcode, $shop_id);
+        $sign = parent::getSign();
+        $suburl = '/returns/get_return_dispute_reason?access_token='.$access_token.'&partner_id='.$this->partnerid.'&return_sn=200203171852695&shop_id='.$shop_id.'&sign='.$sign.'&timestamp='.$this->timest;
+        $response = $this->shopee->getMethod($suburl);
+        return $response;
+    }
+
 
  }
