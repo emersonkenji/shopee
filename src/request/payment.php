b20cd2f0ecf2a5da18d9924bcc2cbed535557c88
Faiz Nurullah <59213454+Faiznurullah@users.noreply.github.com>
2024-08-12 19:42:46 +0700

    commit before merge pull

--- src/request/payment.php
+++ src/request/payment.php
@@ -16,7 +16,82 @@ class payment extends config{
         $this->shopee = new shopee();
     }
 
-    
 
+
+    public function getEscrowDetail($data = []){
+        $suburl = '/payment/get_escrow_detail';
+        $response = $this->shopee->postMethod($suburl, $data);
+        return $response;
+    }
+
+    public function setShopInstallmentStatus($data = []){
+        $suburl = '/payment/set_shop_installment_status ';
+        $response = $this->shopee->getMethodWithPayload($suburl, $data);
+        return $response;
+    } 
+
+    public function getShopInstallmentStatus($data = []){
+        $suburl = '/payment/get_shop_installment_status';
+        $response = $this->shopee->getMethodWithPayload($suburl, $data);
+        return $response;
+    }
+
+    public function getPayoutDetail($data = []){
+        $suburl = '/payment/get_payout_detail';
+        $response = $this->shopee->getMethodWithPayload($suburl, $data);
+        return $response;
+    }
+
+    public function setItemInstallmentStatus($data = []){
+        $suburl = '/payment/set_item_installment_status';
+        $response = $this->shopee->getMethod($suburl, $data);   
+        return $response;
+    }
+
+    public function getItemInstallmentStatus($data = []){
+        $suburl = '/payment/get_item_installment_status';
+        $response = $this->shopee->getMethodWithPayload($suburl, $data);
+        return $response;
+    }
+
+    public function getPaymentMethodList($data = []){
+        $suburl = '/payment/get_payment_method_list';
+        $response = $this->shopee->getMethodWithPayload($suburl, $data);
+        return $response;
+    }
+
+    public function getWalletTransactionList($data = []){
+        $suburl = '/payment/get_wallet_transaction_list';
+        $response = $this->shopee->getMethodWithPayload($suburl, $data);
+        return $response;
+    }
+
+    public function getEscrowList($data = []){
+        $suburl = '/payment/get_escrow_list';
+        $response = $this->shopee->getMethodWithPayload($suburl, $data);
+        return $response;
+    }
+
+    public function getPayoutInfo($data = []){
+        $suburl = '/payment/get_payout_info';
+        $response = $this->shopee->getMethodWithPayload($suburl, $data);
+        return $response;
+    }
+
+    public function getBillingTransactionInfo($authcode, $shop_id, $data = []){
+        $access_token = parent::getAccesToken($authcode, $shop_id);
+        $sign = parent::getSign();
+        $suburl = '/payment/get_billing_transaction_info?access_token='.$access_token.'&partner_id='.$this->partnerid.'&shop_id='.$shop_id.'&sign='.$sign.'&timestamp='.$this->timest;
+        $response = $this->shopee->getMethod($suburl, $data);
+        return $response;
+    }
+
+    public function getEscrowDetailBatch($data = []){
+        $suburl = '/payment/get_escrow_detail_batch';
+        $response = $this->shopee->postMethod($suburl, $data);
+        return $response;
+    }
+
+    
 
 }
--- src/request/return.php
+++ src/request/return.php
@@ -1,1 +1,70 @@
 <?php
+ namespace Faiznurullah\Shopee;
+
+ use Faiznurullah\Shopee\config;
+
+ class payment extends config{
+
+
+    private $config, $partnerid, $partnerkey, $shopee, $access_token;
+    
+    public function __construct($partnerid, $partnerkey)
+    {
+        $this->partnerid = $partnerid;
+        $this->partnerkey = $partnerkey;
+        $this->shopee = new shopee();
+    }
+
+    public function getReturnDetail($data = []){
+       $suburl = '/returns/get_return_detail';
+       $response = $this->shopee->getMethodWithPayload($suburl, $data);
+       return $response;
+    }
+
+
+    public function getReturnList($data = []){
+        $suburl = '/returns/get_return_list';
+        $response = $this->shopee->getMethodWithPayload($suburl, $data);
+        return $response;
+    }
+
+    public function returnConfirm($data = []){ 
+        $suburl = '/returns/confirm';
+        $response = $this->shopee->getMethodWithPayload($suburl, $data);
+        return $response;
+    }
+
+
+    public function dispute($data){
+       $suburl = '/returns/dispute';
+       $response = $this->shopee->postMethod($suburl, $data);
+       return $response;
+    }
+
+    public function getAvailableSolutions($authcode, $shop_id, $return_sn){
+        $access_token = parent::getAccesToken($authcode, $shop_id);
+        $sign = parent::getSign();
+        $suburl = '/returns/get_available_solutions?access_token='.$access_token.'&partner_id='.$this->partnerid.'&return_sn='.$return_sn.'&shop_id='.$shop_id.'&sign='.$sign.'&timestamp='.$this->timest;
+        $response = $this->shopee->getMethod($suburl);
+        return $response;
+    }
+
+    public function offer($authcode, $shop_id, $data = []){
+        $access_token = parent::getAccesToken($authcode, $shop_id);
+        $sign = parent::getSign();
+        $suburl = '/returns/offer?access_token='.$access_token.'&partner_id='.$this->partnerid.'&shop_id='.$shop_id.'&sign='.$sign.'&timestamp='.$this->timest;
+        $response = $this->shopee->postMethod($suburl, $data);
+        return $response;
+    }
+
+    public function acceptOffer($authcode, $shop_id, $data = []){
+        $access_token = parent::getAccesToken($authcode, $shop_id);
+        $sign = parent::getSign();
+        $suburl = '/returns/accept_offer?access_token='.$access_token.'&partner_id='.$this->partnerid.'&shop_id='.$shop_id.'&sign='.$sign.'&timestamp='.$this->timest;
+        $response = $this->shopee->postMethod($suburl, $data);
+        return $response;
+    }
+
+   
+
+ }
